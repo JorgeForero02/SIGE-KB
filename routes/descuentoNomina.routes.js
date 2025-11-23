7eb628dbb015c42a761a911cd31317d242714271
JorgeForero02 <forero07andres@gmail.com>
2025-11-23 15:25:14 -0500

    Refactor routes for Descuento de Nómina to include validation and update authorization roles

--- routes/descuentoNomina.routes.js
+++ routes/descuentoNomina.routes.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const descuentoNominaController = require('../controllers/descuentoNomina.controller');
-const { authenticateToken,  authorize} = require('../middlewares/auth');
+const { authenticateToken, authorize } = require('../middlewares/auth');
+const validate = require('../middlewares/validate');
+const { body, param } = require('express-validator');
 
 router.use(authenticateToken);
-
-router.post('/', authorize('admin', 'gerente'), descuentoNominaController.createDescuentoNomina);
-
-router.get('/:id', descuentoNominaController.getDescuentoNominaById);
-
-router.get('/', descuentoNominaController.getAllDescuentosNomina);
+router.post('/',
+    [
+        body('descripcion').isString().notEmpty(), 
+        body('valor').isFloat({ gt: 0 }),
+        body('fechaDescuento').isISO8601().toDate(),
+        body('idEmpleado').isInt()
+    ],
+    validate,
+    authorize('Administrador', 'Gerente'),
+    descuentoNominaController.createDescuentoNomina
+);
+router.get('/',
+    descuentoNominaController.getAllDescuentosNomina
+);
+router.get('/:id',
+    param('id').isInt(),
+    validate,
+    descuentoNominaController.getDescuentoNominaById
+);
 
 module.exports = router;
